77c5021295bdd38c5b75738b6879a9ea7e215a53
rabiiasultan71-rgb <rabiiasultan71@gmail.com>
2026-06-03 17:03:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,20 @@ import pandas as pd
 import numpy as np
 import os
 from sklearn.ensemble import RandomForestClassifier
+import streamlit as st
+import hashlib
+# --- GİZLEME KODU BURAYA ---
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+# ---------------------------
+
+# ... kodunun geri kalanı ...
 
 # --- ARKA PLAN FONKSİYONU ---
 def set_background(image_file):
